07d7987551b79880d1d56c0be543b54c8f3544bb
GRMARTM <120740684+GRMARTM@users.noreply.github.com>
2023-01-23 19:41:40 +0300

    Домашняя работа ко 2 уроку, задание № 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.Write("Введите порядковый номер дня недели: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+
+// if(number > 5)
+// {
+//     Console.WriteLine("Да");
+// }
+// else 
+// {
+//     Console.WriteLine("НЕТ");
+// }
+
+Console.Write("Введите порядковый номер дня недели : ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 1 && num <=5)
+{
+    Console.Write("Это не выходной");
+}
+if (num >= 6 && num <= 7)
+{
+    Console.Write("Это выходной");
+}
+if (num < 1 || num > 7)
+{
+    Console.Write("Ошибка ввода, попробуйте снова ");
+}
