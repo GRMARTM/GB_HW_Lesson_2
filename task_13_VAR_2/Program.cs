0f1959473647cdada96de3d40fd2242b22876b05
GRMARTM <120740684+GRMARTM@users.noreply.github.com>
2023-01-24 13:02:38 +0300

    Добавил второй вариант решения 13 задачи Циклом

---
+++ task_13_VAR_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+else while ( number >= 1000)
+{
+number = number / 10 ;
+}
+
+
+if  (number > 100 && number < 1000)
+{
+    int digit = number / 10;
+    int digit2 = (number - digit * 10);
+    Console.WriteLine(digit2);
+}
+
